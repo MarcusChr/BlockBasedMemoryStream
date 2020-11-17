b312790d9f7967a242ed3090991aa13b0a9c9e02
MarcusChr <mail@marcuslc.com>
2020-11-17 19:24:24 +0100

    Added comments.

--- BlockBasedMemoryStream.cs
+++ BlockBasedMemoryStream.cs
@@ -16,16 +16,28 @@ namespace com.marcuslc.BlockBasedMemoryStream
 
         private int _bufferSize;
 
+        /// <summary>
+        /// Returns the set Buffer size.
+        /// </summary>
         public int BufferSize
         {
             get => _bufferSize;
         }
 
+        /// <summary>
+        /// Creates a memory stream based on a linked list with fixed size buffers.
+        /// <para/>
+        /// The default buffer-size is 65535 bytes (2^16 - 1).
+        /// </summary>
         public BlockBasedMemoryStream()
         {
             _init(ushort.MaxValue);
         }
 
+        /// <summary>
+        /// Creates a memory stream based on a linked list with custom fixed size buffers.
+        /// </summary>
+        /// <param name="bufferSize">Custom size of the buffers. The bigger the buffer-size is, the faster it is to add, although more memory will be wasted.</param>
         public BlockBasedMemoryStream(int bufferSize)
         {
             _init(bufferSize);
@@ -37,37 +49,66 @@ namespace com.marcuslc.BlockBasedMemoryStream
 
         public override bool CanWrite => true;
 
+        /// <summary>
+        /// Gets the size of the BlockBasedMemoryStream. Be aware, a full loop-through the list will be necessary. 
+        /// </summary>
         public override long Length => _getLength();
 
+        /// <summary>
+        /// Not supported. Will throw a NotSupportedException.
+        /// </summary>
         public override long Position
         {
             get => throw new NotSupportedException();
             set => throw new NotSupportedException();
         }
 
+        /// <summary>
+        /// This method does nothing, has no effect.
+        /// </summary>
         public override void Flush()
         {
             return;
         }
 
+        /// <summary>
+        /// Reads from the BlockBasedMemoryStream.
+        /// </summary>
+        /// <param name="buffer">The destination buffer.</param>
+        /// <param name="offset">The offset in the destination buffer.</param>
+        /// <param name="count">The amount of bytes to read. Must be less or equal than the size of the buffer-parameter minus the offset.</param>
+        /// <returns>Returns the amount of bytes read. </returns>
         public override int Read(byte[] buffer, int offset, int count)
         {
-            if (count > (buffer.Length - offset)) throw new ArgumentOutOfRangeException($"{nameof(count)} was bigger than ({nameof(buffer)}.Length - {nameof(offset)})");
-            if (offset > buffer.Length) throw new ArgumentOutOfRangeException("Offset was bigger than buffer");
-
             return _read(buffer, offset, count, removeReadData: true);
         }
 
+        /// <summary>
+        /// Not supported. You cannot seek in this Stream. Will throw a NotSupportedException.
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <param name="origin"></param>
+        /// <returns></returns>
         public override long Seek(long offset, SeekOrigin origin)
         {
             throw new NotSupportedException();
         }
 
+        /// <summary>
+        /// Sets the length of the stream. The new length cannot be greater than the current length of the Stream.
+        /// </summary>
+        /// <param name="value">The new length</param>
         public override void SetLength(long value)
         {
             _setLength(value);
         }
 
+        /// <summary>
+        /// Writes to the Stream.
+        /// </summary>
+        /// <param name="buffer">The buffer to write data from.</param>
+        /// <param name="offset">An offset on where to begin copying from buffer.</param>
+        /// <param name="count">The maximum amount of bytes to copy.</param>
         public override void Write(byte[] buffer, int offset, int count)
         {
             int bytesWritten = 0;
@@ -99,16 +140,28 @@ namespace com.marcuslc.BlockBasedMemoryStream
             }
         }
 
+        /// <summary>
+        /// Clears the Stream.
+        /// </summary>
         public void Clear()
         {
             _init(_bufferSize);
         }
 
+        /// <summary>
+        /// Returns a byte array with the content of the stream.
+        /// </summary>
+        /// <returns></returns>
         public byte[] ToArray()
         {
             return this.ToArray(false);
         }
 
+        /// <summary>
+        /// Returns a byte array with the content of the stream.
+        /// </summary>
+        /// <param name="removeReadData">Whether or not to remove the returned data from the inner-stream. Default is false.</param>
+        /// <returns></returns>
         public byte[] ToArray(bool removeReadData = false)
         {
             byte[] buffer = new byte[this.Length];
@@ -116,9 +169,13 @@ namespace com.marcuslc.BlockBasedMemoryStream
             return buffer;
         }
 
+        /// <summary>
+        /// Skips a number of bytes.
+        /// </summary>
+        /// <param name="numberOfBytes">The number of bytes to skip.</param>
         public void Skip(int numberOfBytes)
         {
-            this.Read(new byte[numberOfBytes], 0, numberOfBytes); //This might be a temporary solution - who knows?
+            this.Read(new byte[numberOfBytes], 0, numberOfBytes);
         }
 
         private int _read(byte[] buffer, int offset, int count, bool removeReadData = true)
@@ -134,7 +191,7 @@ namespace com.marcuslc.BlockBasedMemoryStream
                 fixed (byte* destPtr = &buffer[offset])
                 {
                     int i = 0;
-                    while(currentIndex <= count && current != null)
+                    while (currentIndex <= count && current != null)
                     {
                         ValueHolder value = current.Value;
                         int bytesToCopyThisRound = (value.end - value.start);
@@ -220,7 +277,7 @@ namespace com.marcuslc.BlockBasedMemoryStream
 
                 ++i;
             }
-            if(newEndPos > current.Value.end)
+            if (newEndPos > current.Value.end)
             {
                 throw exceptionToThrow;
             }
