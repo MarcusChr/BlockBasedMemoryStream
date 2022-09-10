e75c09ca0784491f44ce5806d68ccad387008d08
Marcus Christensen <mail@marcuslc.com>
2022-09-10 21:21:09 +0200

    Moved source project into a sub-folder

---
+++ BlockBasedMemoryStream/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace com.marcuslc.BlockBasedMemoryStream
+{
+    public unsafe class Node
+    {
+        public Node(int bufferSize)
+        {
+            Next = null;
+
+            Value = new ValueHolder
+            {
+                start = 0,
+                pointer = Marshal.AllocHGlobal(bufferSize).ToPointer(),
+                end = 0
+            };
+        }
+
+        public Node Next;
+        public ValueHolder Value;
+
+        ~Node()
+        {
+            Marshal.FreeHGlobal(new IntPtr(Value.pointer));
+        }
+    }
+
+    public unsafe struct ValueHolder
+    {
+        public int start;
+        public void* pointer;
+        public int end;
+    }
+}
