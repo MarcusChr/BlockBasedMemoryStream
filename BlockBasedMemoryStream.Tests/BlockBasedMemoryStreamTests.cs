d6125578ac3906c41260195594136be4de9665ac
Marcus Christensen <mail@marcuslc.com>
2022-09-10 21:51:20 +0200

    Added tests for ToArray and Clear

--- BlockBasedMemoryStream.Tests/BlockBasedMemoryStreamTests.cs
+++ BlockBasedMemoryStream.Tests/BlockBasedMemoryStreamTests.cs
@@ -29,7 +29,7 @@ namespace com.marcuslc.BlockBasedMemoryStream.Tests
         }
 
         [Test]
-        public void Read_CheckIfReadDataIsTheSameAsTheDataWritten_DataEqualsDataWritten([Values] bool isUsingValueCaching, [Values(0, 8)] int poolSize, [Values(64, ushort.MaxValue * 8)] int numberOfBytesToWrite)
+        public void Read_WriteBytes_DataEqualsDataWritten([Values] bool isUsingValueCaching, [Values(0, 8)] int poolSize, [Values(64, ushort.MaxValue * 8)] int numberOfBytesToWrite)
         {
             //Arrange
             var memoryBasedMemoryStream = new BlockBasedMemoryStream(isUsingValueCaching, poolSize);
@@ -43,6 +43,38 @@ namespace com.marcuslc.BlockBasedMemoryStream.Tests
 
             //Assert
             Assert.That(bytesRead, Is.EquivalentTo(bytesToWrite));
+        }
+
+        [Test]
+        public void ToArray_WriteBytes_ToArrayEqualsDataWritten([Values] bool isUsingValueCaching, [Values(0, 8)] int poolSize, [Values(64, ushort.MaxValue * 8)] int numberOfBytesToWrite)
+        {
+            //Arrange
+            var memoryBasedMemoryStream = new BlockBasedMemoryStream(isUsingValueCaching, poolSize);
+            byte[] bytesToWrite = new byte[numberOfBytesToWrite];
+            _sharedRandom.NextBytes(bytesToWrite);
+
+            //Act
+            memoryBasedMemoryStream.Write(bytesToWrite, 0, bytesToWrite.Length);
+            byte[] bytesRead = memoryBasedMemoryStream.ToArray();
+
+            //Assert
+            Assert.That(bytesRead, Is.EquivalentTo(bytesToWrite));
+        }
+
+        [Test]
+        public void Clear_WriteBytesThenClear_LengthEqualsZero([Values] bool isUsingValueCaching, [Values(0, 8)] int poolSize, [Values(64, ushort.MaxValue * 8)] int numberOfBytesToWrite)
+        {
+            //Arrange
+            var memoryBasedMemoryStream = new BlockBasedMemoryStream(isUsingValueCaching, poolSize);
+            byte[] bytesToWrite = new byte[numberOfBytesToWrite];
+            _sharedRandom.NextBytes(bytesToWrite);
+
+            //Act
+            memoryBasedMemoryStream.Write(bytesToWrite, 0, bytesToWrite.Length);
+            memoryBasedMemoryStream.Clear();
+
+            //Assert
+            Assert.That(memoryBasedMemoryStream.Length, Is.EqualTo(0));
         }
     }
 }
