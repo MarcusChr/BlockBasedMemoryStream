2e5a0a2002340f52713331771bb0879bba460658
Marcus Christensen <mail@marcuslc.com>
2022-09-11 19:03:59 +0200

    Added tests for SetLength, Seek, Dispose and Skip

--- BlockBasedMemoryStream.Tests/BlockBasedMemoryStreamTests.cs
+++ BlockBasedMemoryStream.Tests/BlockBasedMemoryStreamTests.cs
@@ -128,5 +128,77 @@ namespace com.marcuslc.BlockBasedMemoryStream.Tests
             //Assert
             Assert.That(targetMemoryStream.ToArray(), Is.EquivalentTo(bytesToWrite));
         }
+
+        [Test]
+        public void SetLength_WritesBytesThenSetsLength_LengthEqualsTheSetLength([Values] bool isUsingValueCaching, [Values(0, 8)] int poolSize, [Values(64, ushort.MaxValue * 8)] int numberOfBytesToWrite)
+        {
+            //Arrange
+            var memoryBasedMemoryStream = new BlockBasedMemoryStream(isUsingValueCaching, poolSize);
+            var targetLength = numberOfBytesToWrite / 2;
+
+            byte[] bytesToWrite = new byte[numberOfBytesToWrite];
+
+            _sharedRandom.NextBytes(bytesToWrite);
+
+            //Act
+            memoryBasedMemoryStream.Write(bytesToWrite, 0, bytesToWrite.Length);
+            memoryBasedMemoryStream.SetLength(targetLength);
+
+            //Assert
+            Assert.That(memoryBasedMemoryStream.Length, Is.EqualTo(targetLength));
+        }
+
+        [Test]
+        public void Seek_AttemptsToCallTheSeekMethod_ANotSupportedExceptionIsThrown([Values] bool isUsingValueCaching, [Values(0, 8)] int poolSize)
+        {
+            //Arrange
+            var memoryBasedMemoryStream = new BlockBasedMemoryStream(isUsingValueCaching, poolSize);
+
+            //Act
+            var exceptionThrown = Assert.Throws<NotSupportedException>(() => memoryBasedMemoryStream.Seek(0, SeekOrigin.Begin));
+
+            //Assert
+            Assert.That(exceptionThrown, Is.Not.Null);
+        }
+
+        [Test] //Another exception should be thrown. NullReferenceException is thrown because 'Dispose()' sets an internal field variable to null, which is then accessed when trying to write to the stream.
+        public void Dispose_AttempsToWriteAfterDisposingStream_AnExceptionIsThrown([Values] bool isUsingValueCaching, [Values(0, 8)] int poolSize, [Values(64, ushort.MaxValue * 8)] int numberOfBytesToWrite)
+        {
+            //Arrange
+            var memoryBasedMemoryStream = new BlockBasedMemoryStream(isUsingValueCaching, poolSize);
+            byte[] bytesToWrite = new byte[numberOfBytesToWrite];
+            _sharedRandom.NextBytes(bytesToWrite);
+
+            //Act
+            memoryBasedMemoryStream.Write(bytesToWrite);
+            memoryBasedMemoryStream.Dispose();
+            var exceptionThrown = Assert.Throws<NullReferenceException>(() => memoryBasedMemoryStream.Write(bytesToWrite));
+
+            //Assert
+            Assert.That(exceptionThrown, Is.Not.Null);
+        }
+
+        [Test]
+        public void Skip_WritesBytesSkipsThenReadsTheBytes_TheReadBytesShouldEqualTheBytesReadExcludingTheSkippedBytes([Values] bool isUsingValueCaching, [Values(0, 8)] int poolSize, [Values(64, ushort.MaxValue * 8)] int numberOfBytesToWrite, [Values(16, 32)] int numberOfBytesToSkip)
+        {
+            //Arrange
+            var memoryBasedMemoryStream = new BlockBasedMemoryStream(isUsingValueCaching, poolSize);
+            var numberOfExpectedBytes = numberOfBytesToWrite - numberOfBytesToSkip;
+
+            byte[] bytesToWrite = new byte[numberOfBytesToWrite];
+            byte[] bytesRead = new byte[numberOfExpectedBytes];
+            byte[] expectedBytes = new byte[numberOfExpectedBytes];
+
+            _sharedRandom.NextBytes(bytesToWrite);
+            Array.Copy(bytesToWrite, numberOfBytesToSkip, expectedBytes, 0, expectedBytes.Length);
+
+            //Act
+            memoryBasedMemoryStream.Write(bytesToWrite, 0, bytesToWrite.Length);
+            memoryBasedMemoryStream.Skip(numberOfBytesToSkip);
+            memoryBasedMemoryStream.Read(bytesRead, 0, bytesRead.Length);
+
+            //Assert
+            Assert.That(bytesRead, Is.EquivalentTo(expectedBytes));
+        }
     }
 }
